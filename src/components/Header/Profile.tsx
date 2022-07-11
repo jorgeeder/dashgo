ea87f22062ee0536c00e9f92c4e9441294f308cb
jorgeeder <jorgeeder.js@gmail.com>
2022-07-11 09:42:54 -0300

    feat: Responsive Header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean,
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Jorge Eder</Text>
-                <Text color="gray.300" fontSize="small">
-                    jorgeeder.dev@gmail.com
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box mr="4" textAlign="right">
+                    <Text>Jorge Eder</Text>
+                    <Text color="gray.300" fontSize="small">
+                        jorgeeder.dev@gmail.com
+                    </Text>
+                </Box>
+            )}
 
             <Avatar size="md" name="Jorge Eder" src="https://github.com/jorgeeder.png" />
         </Flex>
