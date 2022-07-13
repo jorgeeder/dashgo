f7add2dc66b79d761abeb6e60ddca7f1f00f0bdf
jorgeeder <jorgeeder.js@gmail.com>
2022-07-13 06:17:28 -0300

    feat: Responsive registration

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -6,7 +6,8 @@ import {
     Heading,
     HStack,
     SimpleGrid,
-    VStack} from '@chakra-ui/react';
+    VStack
+} from '@chakra-ui/react';
 import { Input } from '../../components/Form/Input';
 
 import { Header } from '../../components/Header';
@@ -20,23 +21,23 @@ export default function UserList() {
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
 
-                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
                     <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
 
                     <Divider my="6" borderColor="gray.700" />
-                    
+
                     <VStack spacing="8">
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                             <Input name="name" label="Nome completo"></Input>
                             <Input name="email" type="email" label="E-mail"></Input>
                         </SimpleGrid>
 
-                        <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+                        <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
                             <Input name="password" type="password" label="Senha"></Input>
                             <Input name="password_confirmation" type="password" label="Confirmação da senha"></Input>
                         </SimpleGrid>
                     </VStack>
-                    
+
                     <Flex mt="8" justify="flex-end">
                         <HStack spacing="4">
                             <Button colorScheme="whiteAlpha">Cancelar</Button>
