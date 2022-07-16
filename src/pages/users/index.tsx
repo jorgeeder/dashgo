e5e34c4a4d968d41c085596ca62b2c20ca080761
jorgeeder <jorgeeder.js@gmail.com>
2022-07-16 09:59:40 -0300

    feat: Configuring React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
     Flex,
     Heading,
     Icon,
+    Spinner,
     Table,
     Tbody,
     Td,
@@ -20,19 +21,23 @@ import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 import Link from "next/link"
 import { useEffect } from 'react';
+import { useQuery } from 'react-query';
 
 export default function UserList() {
+    const { data, isLoading, error } = useQuery("users", async () => {
+        const response = await fetch("http://localhost:3000/api/users")
+        const data = await response.json();
+
+        return data;
+    })
+
 
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
     });
 
-    useEffect(() => {
-        fetch("http://localhost:3000/api/users")
-        .then(response => response.json()
-        .then(data => console.log(data)))
-    },[])
+
 
     return (
         <Box>
@@ -58,67 +63,79 @@ export default function UserList() {
                         </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuários</Th>
-                                {isWideVersion && <Th>Data de Cadastro</Th>}
-                                <Th width="8"></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Jorge Eder</Text>
-                                        <Text fontSize="sm" color="gray.300">jorgeeder.dev@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>25 de Dezembro, 2022 </Td>}
-                                <Td>
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários.</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuários</Th>
+                                        {isWideVersion && <Th>Data de Cadastro</Th>}
+                                        <Th width="8"></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink" />
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">Jorge Eder</Text>
+                                                <Text fontSize="sm" color="gray.300">jorgeeder.dev@gmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>25 de Dezembro, 2022 </Td>}
+                                        <Td>
 
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Jorge Eder</Text>
-                                        <Text fontSize="sm" color="gray.300">jorgeeder.dev@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>25 de Dezembro, 2022 </Td>}
-                                <Td>
+                                        </Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink" />
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">Jorge Eder</Text>
+                                                <Text fontSize="sm" color="gray.300">jorgeeder.dev@gmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>25 de Dezembro, 2022 </Td>}
+                                        <Td>
 
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Jorge Eder</Text>
-                                        <Text fontSize="sm" color="gray.300">jorgeeder.dev@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>25 de Dezembro, 2022 </Td>}
-                                <Td>
+                                        </Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td px={["4", "4", "6"]}>
+                                            <Checkbox colorScheme="pink" />
+                                        </Td>
+                                        <Td>
+                                            <Box>
+                                                <Text fontWeight="bold">Jorge Eder</Text>
+                                                <Text fontSize="sm" color="gray.300">jorgeeder.dev@gmail.com</Text>
+                                            </Box>
+                                        </Td>
+                                        {isWideVersion && <Td>25 de Dezembro, 2022 </Td>}
+                                        <Td>
 
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
+                                        </Td>
+                                    </Tr>
+                                </Tbody>
+                            </Table>
 
-                    <Pagination />
+                            <Pagination />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
