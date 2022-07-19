c15b2a3202828fe278041e6a1c690850d6ad75b1
jorgeeder <jorgeeder.js@gmail.com>
2022-07-19 14:50:22 -0300

    feat: Switching Pages

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,9 +22,11 @@ import { Sidebar } from '../../components/Sidebar';
 import Link from "next/link"
 
 import { useUsers } from '../../services/hooks/useUsers';
+import { useState } from 'react';
 
 export default function UserList() {
-    const { data, isLoading, isFetching, error } = useUsers();
+    const [page, setPage] = useState(1);
+    const { data, isLoading, isFetching, error } = useUsers(page);
 
 
     const isWideVersion = useBreakpointValue({
@@ -83,7 +85,7 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    {data.map(user => {
+                                    {data.users.map(user => {
                                         return (
                                             <Tr key={user.id}>
                                                 <Td px={["4", "4", "6"]}>
@@ -107,9 +109,9 @@ export default function UserList() {
                             </Table>
 
                             <Pagination
-                                totalCountOfRegisters={200}
-                                currentPage={5}
-                                onPageChange={() => { }}
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
                             />
                         </>
                     )}
